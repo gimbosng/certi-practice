644641c228ff9fc6847c229ff8e81419f1d1e4b6
bosngkim <gimbosng@gmail.com>
2025-09-16 23:45:28 +0900

    [Stack] 깊이 우선 탐색, 백트래킹 기법, 대표문제-미로찾기 (DFS)

---
+++ certi-practice/DFS/DFS.cpp
@@ -0,0 +1,65 @@
+﻿// DFS.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int stack[8] = { 0, };
+int top = -1;
+int visited[8] = { 0, };
+int arr[8][8] = { 0, };
+
+void push(int value)
+{
+    printf("%d", value);
+    stack[++top] = value;
+    visited[value] = 1;
+    return;
+}
+
+void pop()
+{
+    top--;
+    return;
+}
+
+void dfs(int value)
+{
+    push(value);
+
+    while (top >= 0)
+    {
+        for (int i = 1; i < 8; i++)
+        {
+            if (visited[i] == 0 && arr[stack[top]][i] == 1)
+            {
+                push(i);
+                i = 1;
+            }
+        }
+        pop();
+    }
+}
+
+int main()
+{
+    int a, b;
+    for (int i = 0; i < 7; i++)
+    {
+        scanf_s("%d %d", &a, &b);
+        arr[a][b] = 1;
+        arr[b][a] = 1;
+    }
+    dfs(1);
+    return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
