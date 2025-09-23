2889e1df000f7fbd0ce76da23f2085f0adb2631c
bosngkim <gimbosng@gmail.com>
2025-09-23 22:12:33 +0900

    [Queue] 큐, 대표문제-BFS

---
+++ certi-practice/BFS/BFS.cpp
@@ -0,0 +1,54 @@
+﻿// BFS.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int frontIndex = -1, rearIndex = -1;
+int arrSize;
+int arr[100];
+
+bool IsEmpty()
+{
+    return (frontIndex == rearIndex);
+}
+
+bool IsFull()
+{
+    return (rearIndex == arrSize);
+}
+
+void enQueue(int item)
+{
+    if (IsFull()) return;
+    rearIndex++;
+    arr[rearIndex] = item;
+}
+
+int deQueue()
+{
+    if (IsEmpty()) return -1;
+    frontIndex++;
+    return arr[frontIndex];
+}
+
+int peekQueue()
+{
+    if (IsEmpty()) return -1;
+    return arr[frontIndex + 1];
+}
+
+int main()
+{
+    
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
