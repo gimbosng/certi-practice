f709dba31c33f7a9de5de7e7008fdcce6d7ed5d1
bosngkim <gimbosng@gmail.com>
2025-11-06 22:50:27 +0900

    Update 퇴사.cpp

--- backjoon/퇴사/퇴사.cpp
+++ backjoon/퇴사/퇴사.cpp
@@ -2,72 +2,25 @@
 #include <algorithm>
 using namespace std;
 
-int N, M;
-int board[500][500];
-bool visited[500][500];
-int answer = 0;
-
-// 상하좌우 방향
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
-
-void dfs(int x, int y, int depth, int sum) {
-    if (depth == 4) {
-        answer = max(answer, sum);
-        return;
-    }
-
-    for (int dir = 0; dir < 4; ++dir) {
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-
-        if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-        if (visited[nx][ny]) continue;
-
-        visited[nx][ny] = true;
-        dfs(nx, ny, depth + 1, sum + board[nx][ny]);
-        visited[nx][ny] = false;
-    }
-}
-
-// DFS로 만들 수 없는 'ㅗ' 모양 처리
-void check_t_shape(int x, int y) {
-    int center = board[x][y];
-    for (int i = 0; i < 4; ++i) {
-        int temp = center;
-        bool valid = true;
-        for (int j = 0; j < 3; ++j) {
-            int dir = (i + j) % 4;
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-                valid = false;
-                break;
-            }
-            temp += board[nx][ny];
-        }
-        if (valid) answer = max(answer, temp);
-    }
-}
+int N;
+int T[16], P[16], dp[16];
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> N >> M;
+    cin >> N;
     for (int i = 0; i < N; ++i)
-        for (int j = 0; j < M; ++j)
-            cin >> board[i][j];
+        cin >> T[i] >> P[i];
 
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            visited[i][j] = true;
-            dfs(i, j, 1, board[i][j]);
-            visited[i][j] = false;
-            check_t_shape(i, j);
-        }
+    for (int i = N - 1; i >= 0; --i) {
+        int next_day = i + T[i];
+        if (next_day <= N)
+            dp[i] = max(P[i] + dp[next_day], dp[i + 1]);
+        else
+            dp[i] = dp[i + 1];
     }
 
-    cout << answer << '\n';
+    cout << dp[0] << '\n';
     return 0;
 }
